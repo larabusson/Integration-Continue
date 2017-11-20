1f1d2a25218f12ea0fd23ea43845d00c23c42d14
larabusson <larabusson@github.com>
2017-11-20 19:12:13 +0100

    bla

--- supprimer.php
+++ supprimer.php
@@ -1,9 +1,4 @@
 <?php
-session_start();
-$contenu = json_decode(file_get_contents("list_conf.json"));
-if ($contenu!=NULL){
-  foreach($contenu as $key => $d){
-     $tab[$key] = $d;
 
   $contenu = json_decode(file_get_contents("list_conf.json"));
   if ($contenu!=NULL){
@@ -11,20 +6,12 @@ if ($contenu!=NULL){
        $tab[$key] = $d;
     }
   }
-}
 
- unset($tab[$_GET['id']]);
- $contenu = json_encode($tab);
- $fichier = fopen("list_conf.json", 'w+');
- fwrite($fichier, $contenu);
    unset($tab[$_GET['id']]);
    $contenu = json_encode($tab);
    $fichier = fopen("list_conf.json", 'w+');
    fwrite($fichier, $contenu);
 
- // Fermeture du fichier
- fclose($fichier);
- header('Location: ./home.php');
    // Fermeture du fichier
    fclose($fichier);
    header('Location: ./home.php');
