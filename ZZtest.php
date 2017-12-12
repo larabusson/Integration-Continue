8152df7f43f03115d52e44c614d9947952fc4061
larabusson <larabusson@github.com>
2017-12-12 16:20:17 +0100

    4

---
+++ ZZtest.php
@@ -0,0 +1,25 @@
+<?php
+require_once("./functions/functin.php");
+class TestCalc extends  PHPUnit_Framework_TestCase {
+
+  public function setup(){
+    echo "Debut du test";
+  }
+
+
+  public function Conf(){
+      $c=new Conference();
+      $c->title = "Conference 1"
+      $this->assertTrue($c->title=="Conference 1", "OK");
+  }
+
+  public function tearDown(){
+    echo"fin du test";
+  }
+
+}
+
+
+
+
+?>
