34f24d0a645c779426dd868fe25c8108a6b1be9f
larabusson <larabusson@github.com>
2017-12-12 16:25:33 +0100

    4

--- Test.php
+++ Test.php
@@ -10,12 +10,10 @@ class TestCalc extends  PHPUnit_Framework_TestCase {
   public function Conf(){
       $c=new Conference();
       $c->title = "Conference 1"
-      assertTrue($c->title=="Conference 1", "OK");
+      $this->assertTrue($c->title=="Conference 1", "OK");
   }
 
-  public function tearDown(){
-    echo"fin du test";
-  }
+  
 
 }
 
