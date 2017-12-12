2e82521681d60dc68d1fd9c21a7a8b3bed5a309a
larabusson <larabusson@github.com>
2017-12-12 16:08:48 +0100

    4

--- Test.php
+++ Test.php
@@ -8,11 +8,33 @@ class TestCalc extends  PHPUnit_Framework_TestCase {
 
 
   public function essai(){
-    $this->assertTrue(1==1, "Test reussi");
+    $this->assertTrue(1==1, "Test reussi")
   }
 
 
-  
+  public function testMul(){
+    $a = 2 ;
+    $b = 3;
+
+    $c = 6;
+
+    $this->assertTrue($c==6, "Test reussi");
+  }
+
+  public function testDiv(){
+    $a = 4 ;
+    $b = 2;
+    $d = 0 ;
+
+    $c = 2;
+
+    $this->assertTrue($c==2, "Test reussi");
+
+  }
+
+  public function tearDown(){
+    echo"fin du test";
+  }
 
 }
 
