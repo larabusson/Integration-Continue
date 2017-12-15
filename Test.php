55982012d24cb1ca9b6e035ad979f42ca6d4080f
larabusson <larabusson@github.com>
2017-12-15 18:54:01 +0100

    coucou

--- Test.php
+++ Test.php
@@ -15,25 +15,22 @@ class MesTests extends  PHPUnit_Framework_TestCase {
       $tab['time']="10-10";
       $tab['description']="";
       $c=creerConference($tab);
-      $conf['title']="test";
-      $conf['location']="";
-      $conf['author']="";
-      $conf['date']="2050-02-03";
-      $conf['time']="10-10";
-      $conf['description']="";
-      AjoutConference($conf, "./texte/Conf_test.json");
       $this->assertTrue(strcmp($c->title, "test")==0, "OK");
-      supprimer("201802020200003", "./texte/list_conf.json");
+
+
+
+
+      AjoutConference($tab, "./texte/Conf_test.json");
       $tab = Tableau_Conf("./texte/list_conf.json");
       $NB = count($tab);
-      $this->assertTrue(3==$NB, "OK");
-/*$Tableau_conf = Tableau_Conf("./texte/Conf_test.json");
-      $NB = count($Tableau_conf);
-      $this->assertTrue(0==$NB, "OK");
-      $key = creer_clef($tab);
-      echo $key;
-      echo array_key_exists($key , $Tableau_conf);
-      $this->assertTrue(array_key_exists($key , $Tableau_conf));*/
+      $this->assertTrue(5==$NB, "OK");
+
+      supprimer("201802020200003", "./texte/Conf_test.json");
+      $tab = Tableau_Conf("./texte/list_conf.json");
+      $NB = count($tab);
+      $this->assertTrue(4==$NB, "OK");
+
+    /*  $this->assertTrue(array_key_exists($key , $Tableau_conf));*/
   }
 
   public function test_Lecture_Fichier(){
