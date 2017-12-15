6e40136eee1c68b21ceee3a8fab7e036dcfb7869
larabusson <larabusson@github.com>
2017-12-15 18:06:26 +0100

    azekrh

--- functions/function.php
+++ functions/function.php
@@ -13,14 +13,15 @@ public $date;
 
 
 function AjoutConference($d, $chemin){
-  $tableau_conf = array();
+  $tab = array();
   $c = creerConference($d);
-  $tableau_conf=tableau_confleau_Conf($chemin);
-  $NombreConf = count($tableau_conf);
+  $tab=Tableau_Conf($chemin);
+  if($tab){
+  $NombreConf = count($tab);
   $key= creer_clef($d, $NombreConf);
-  $tableau_conf[$key] = $c ;
-  ksort($tableau_conf);
-  $contenu = json_encode($tableau_conf);
+  $tab[$key] = $c ;
+  ksort($tab);
+  $contenu = json_encode($tab);
   $fichier = fopen($chemin, 'w+');
 
   // Ecriture dans le fichier
@@ -29,20 +30,21 @@ function AjoutConference($d, $chemin){
   // Fermeture du fichier
   fclose($fichier);
 }
+}
 
 function creer_clef($d, $NombreConf){
   $key = substr($d['date'], 0, 4).substr($d['date'], 5, 2).substr($d['date'], 8, 2).substr($d['time'], 0, 2).substr($d['time'], 3, 2).str_pad($NombreConf, 3, '0', STR_PAD_LEFT);
   return  $key;
 }
 
-function tableau_confleau_Conf($chemin){
+function Tableau_Conf($chemin){
   $contenu = json_decode(file_get_contents($chemin));
   if ($contenu!=NULL){
     foreach($contenu as $key => $d){
-       $tableau_conf[$key] = $d;
+       $tab[$key] = $d;
     }
   }
-  return $tableau_conf ;
+  return $tab ;
 }
 
 
@@ -61,12 +63,12 @@ function supprimer($i){
   $contenu = json_decode(file_get_contents("../texte/list_conf.json"));
   if ($contenu!=NULL){
     foreach($contenu as $key => $d){
-       $tableau_conf[$key] = $d;
+       $tab[$key] = $d;
     }
   }
 
-   unset($tableau_conf[$i]);
-   $contenu = json_encode($tableau_conf);
+   unset($tab[$i]);
+   $contenu = json_encode($tab);
    $fichier = fopen("../texte/list_conf.json", 'w+');
    fwrite($fichier, $contenu);
 
